95ebe797204098e334f19fe7a303d591db7caca2
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-03-16 22:06:25 +0200

    Created car object model and its view

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class Car(models.Model):
+    brand = models.CharField(max_length=100, )
+    CATEGORY = (
+        ('New', 'New'),
+        ('Used', 'Used')
+    )
+    category = models.CharField(max_length=50, choices=CATEGORY)
+    image_main = models.ImageField(upload_to='images')
+    image1 = models.ImageField(upload_to='images', blank=True)
+
+    def __str__(self):
+        return self.brand
+
+    def get_absolute_url(self):
+        return reverse('car_detail', kwargs={'car_id':self.id})
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Car
+
 
 # Create your views here.
+def index(request):
+    new_cars = Car.objects.filter(category='New')  # TODO:Limit the number of cars that appear on a single page
+    used_cars = Car.objects.filter(category='Used')
+    # To include latest cars
+    all = Car.objects.all()
+
+    # TODO: Add custom filter
+
+    return render(request, 'index.html')
+
+    # TODO: Filter results
+    # TODO: FilterByCategory
+
+
+def about(request):
+    return render(request, 'about.html')
