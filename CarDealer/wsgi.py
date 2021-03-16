7dd621ab5e28a1457d540742655282773544e1dd
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-03-16 18:58:24 +0200

    Refactored folders

--- CarDealer/wsgi.py
+++ CarDealer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CarDealerSite project.
+WSGI config for CarDealer project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CarDealerSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CarDealer.settings')
 
 application = get_wsgi_application()
