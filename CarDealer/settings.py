79340181adc5811fc30c46c0c3fb4187f274e42d
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-03-25 20:51:05 +0200

    fixed static files path

--- CarDealer/settings.py
+++ CarDealer/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static_in_project']
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR /
+                    'static_cdn_local']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / '/media/'
