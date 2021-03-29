6764fe65b0cfac33d304c0cb3f4ee68e827e8e03
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-03-29 21:54:18 +0200

    Added functions to create users

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name=None, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+        user_obj = self.model(
+            email=self.normalize_email(email),
+            full_name=full_name
+        )
+        user_obj.set_password(password)
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.is_active = is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staff_user(self, email, full_name=None, password=None):
+        user = self.create_user(
+            email,
+            full_name=full_name,
+            password=password,
+            is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, full_name=None, password=None):
+        user = self.create_user(
+            email=email,
+            full_name=full_name,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
