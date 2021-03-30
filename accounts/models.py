96bbea50414f2994355217b4f1939737c6a44733
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-03-30 21:49:00 +0200

    Added functions to create users and email activation manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,16 @@
+from datetime import timedelta
+
+from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-
 # Create your models here.
+from django.utils import timezone
+
+DEFAULT_ACTIVATION_DAYS = getattr(settings, 'DEFAULT_ACTIVATION_DAYS', 7)
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, full_name=None, password=None, is_active=True, is_staff=False, is_admin=False):
@@ -40,3 +47,75 @@ class UserManager(BaseUserManager):
             is_admin=True
         )
         return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        if self.full_name:
+            return self.full_name
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def has_permission(self, permission, obj=None):
+        return True
+
+    def has_module_permissions(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        if self.admin:
+            return True
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+
+class EmailActivationQuerySet(models.query.QuerySet):
+    def confirmable(self):
+        now = timezone.now()
+        start_range = now - timedelta(days=DEFAULT_ACTIVATION_DAYS)
+        end_range = now
+
+        return self.filter(
+            activated=False,
+            forced_expired=False
+        ).filter(
+            timestamp__gt=start_range,
+            timestamp__lte=end_range
+        )
+
+
+class EmailActivationManager(models.Manager):
+    def get_queryset(self):
+        return EmailActivationQuerySet(self.model, using=self._db)
+
+    def confirmable(self):
+        return self.get_queryset().confirmable()
+
+    def email_exists(self, email):
+        return self.get_queryset().filter(
+            Q(email=email) |
+            Q(user__email=email)
+        ).filter(
+            activated=False
+        )
