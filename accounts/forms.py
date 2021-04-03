23087275ba81f8e1c3320f892df62f752c1591bd
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-04-03 17:42:38 +0200

    register form added

--- accounts/forms.py
+++ accounts/forms.py
@@ -137,3 +137,32 @@ class LoginForm(forms.Form):
         login(request, user)
         self.user = user
         return data
+
+
+class RegisterForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    :arg
+    :return
+    """
+
+    class Meta:
+        model = User
+        fields = ('full_name', 'email',)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Password don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.is_active = False
+
+        if commit:
+            user.save()
+        return user
