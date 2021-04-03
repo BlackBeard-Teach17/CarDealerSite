7907cf8589aaafc2d25c580bcd524dc018f6c1f4
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-04-03 17:35:24 +0200

    login form added

--- accounts/forms.py
+++ accounts/forms.py
@@ -97,3 +97,43 @@ class GuestForm(forms.ModelForm):
             request = self.request
             request.session['guest_email.id'] = obj.id
         return obj
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        request = self.request
+        data = self.cleaned_data
+        email = data.get("email")
+        password = data.get("password")
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            not_active = qs.filter(is_active=False)
+            if not_active.exists():
+                link = reverse("account:resend-activation")
+                reconfirm_msg = """Go to <a href='{resend_link}'>
+                resend confirmation email</a>
+                """.format(resend_link=link)
+                confirm_email = EmailActivation.objects.filter(email=email)
+                is_confirmable = confirm_email.confirmable().exists()
+                if is_confirmable:
+                    msg1 = "Please check your email to confirm your account or " + reconfirm_msg.lower()
+                    raise forms.ValidationError(mark_safe(msg1))
+                email_confirm_exists = EmailActivation.objects.email_exists(email).exists()
+                if email_confirm_exists:
+                    msg2 = "Email not confirmed" + reconfirm_msg
+                    raise forms.ValidationError(mark_safe(msg2))
+                if not is_confirmable and not email_confirm_exists:
+                    raise forms.ValidationError("This user is inactive")
+        user = authenticate(request, username=email, password=password)
+        if user is None:
+            raise forms.ValidationError("Invalid credentials")
+        login(request, user)
+        self.user = user
+        return data
