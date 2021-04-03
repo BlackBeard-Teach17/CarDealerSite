03728094038c3fa178d4d6cb87d4f2691f14ed26
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2021-04-03 12:46:47 +0200

    unique key and slug generators

--- CarDealer/utils.py
+++ CarDealer/utils.py
@@ -13,3 +13,31 @@ def get_filename(path):
 
 def random_string_generator(size=20, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+
+def unique_key_generator(instance):
+    size = random.randint(30, 45)
+    key = random_string_generator(size=size)
+
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(key=key).exists()
+    if qs_exists:
+        return unique_slug_generator(instance)
+
+
+def unique_slug_generator(instance, new_slug=None):
+
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title)
+
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(slug=slug).exists()
+    if qs_exists:
+        new_slug = "{slug}-{randstr}".format(
+            slug=slug,
+            randstr=random_string_generator(size=4)
+        )
+        return unique_slug_generator(instance, new_slug=new_slug)
+    return
